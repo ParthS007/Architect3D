68a18b2d3f5d51c7f6317e29835202acbc9fa45a
Dogukan Bagci <dogukan.bagci@outlook.com>
2025-04-12 10:08:22 +0200

    gitignore

--- Mask3D/scannetpp.sh
+++ Mask3D/scannetpp.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-#SBATCH --time=47:59:59
 #SBATCH --account=3dv
-#SBATCH --output=mask3d.out
+#SBATCH --job-name=mask3d
+#SBATCH --output=./jobs/mask3d.out
+#SBATCH --error=./jobs/mask3d.err
+#SBATCH --time=47:59:59
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=32G
 
-export OMP_NUM_THREADS=3  # speeds up MinkowskiEngine
 
-CURR_DBSCAN=0.95
-CURR_TOPK=300
-CURR_QUERY=150
-CURR_T=0.001
+# Set PyTorch memory optimization flags
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_VISIBLE_DEVICES=0
 
-# TRAIN python -m datasets.preprocessing.scannetpp_preprocessing preprocess --data_dir="/work/scratch/dbagci" --save_dir="/work/scratch/dbagci/processed/scannetpp" \
-python main_instance_segmentation.py \
+export CURR_DBSCAN=0.95
+export CURR_TOPK=300
+export CURR_QUERY=150
+export CURR_T=0.001
+
+
+srun python main_instance_segmentation.py \
 general.experiment_name="train" \
 general.project_name="scannetpp_train" \
 data/datasets=scannetpp \
 general.eval_on_segments=true \
 general.train_on_segments=true \
 data.train_mode=train
+
+
 
